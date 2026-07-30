8ae9256010454136ff984ad554c8bc18afb18042
Thiago <rohaly1310thiago@gmail.com>
2026-07-30 11:15:21 -0300

    se corrigió la estructura para nuevos estilos

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -2,35 +2,32 @@
 
 require_once __DIR__ . "/../config/menu.php";
 
-
 $rol = $_SESSION['usuario']['rol'];
+
+$rutaActual = $_SERVER['REQUEST_URI'];
 
 ?>
 
-
 <aside class="sidebar">
 
+    <?php foreach ($menu[$rol] as $item) {
 
-    <?php foreach ($menu[$rol] as $item) { ?>
+        $activo = strpos($rutaActual, $item[2]) !== false;
 
+    ?>
 
-        <a href="<?= $item[2] ?>">
-
+        <a
+            href="<?= $item[2] ?>"
+            class="<?= $activo ? 'active' : '' ?>">
 
             <i class="<?= $item[0] ?>"></i>
 
-
             <span>
-
                 <?= $item[1] ?>
-
             </span>
-
 
         </a>
 
-
     <?php } ?>
-
 
 </aside>
